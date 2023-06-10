d34890ca0a4d90e80d097a15b5afa6c75572aca7
Shubham Sareliya <sareliashubham9999@gmail.com>
2023-06-10 16:44:55 +0530

    Final Commit

---
+++ src/components/Task.jsx
@@ -0,0 +1,23 @@
+import { FaRegTrashAlt } from 'react-icons/fa'
+
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`task ${task.reminder ? 'reminder' : ''}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text}
+        <FaRegTrashAlt
+          style={{
+            color: 'red',
+            cursor: 'pointer',
+          }}
+          onClick={() => onDelete(task.id)}
+        /></h3>
+      <p>{task.day}</p>
+    </div >
+  )
+}
+
+export default Task
